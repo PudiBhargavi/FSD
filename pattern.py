369de1a67b4b5c1a848738bbcaaf293072d1a7df
PudiBhargavi <pudibhargavi6002@gmail.com>
2025-03-05 09:57:28 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,10 @@
+def print_pattern(n):
+  for i in range(1, n + 1):
+    for j in range(1, n + 1):
+      if i == j:
+        print("*", end=" ")
+      else:
+        print(j, end=" ")
+    print()
+
+print_pattern(4)
